8dfa1f2c5e815fc1ae99de2f7cf389f3167dfa4a
Pedro Endo <endopedro@gmail.com>
2021-05-24 15:42:08 -0300

    feat: index fetch movies

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,74 @@
-import React, { useEffect } from 'react'
-import moviesApi from '../services/moviesApi'
+import React, { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import Loader from 'react-loader-spinner'
+import Moment from 'react-moment'
 
+import moviesApi from '../services/moviesApi'
 import Layout from '../components/Layout'
+import MovieCard from '../components/MovieCard'
 
 const index = () => {
-  useEffect(() => {
-    moviesApi()
-      .getPopular()
-      .then((res) => console.log(res.data))
-  }, [])
+  const MySwal = withReactContent(Swal)
+
+  const [movies, setMovies] = useState([])
+  const [hasMoreMovies, setHasMoreMovies] = useState(true)
+
+  const fetchMovies = (page) => {
+    if (hasMoreMovies) {
+      moviesApi()
+        .getPopular(page)
+        .then(({ data }) => {
+          setMovies([...movies, ...data.results])
+          if (data.page == data.total_pages) setHasMoreMovies(false)
+        })
+        .catch((error) => {
+          if (error.response.status != 422) {
+            MySwal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Failed to connect to server.',
+            })
+          }
+          setHasMoreMovies(false)
+        })
+    }
+  }
 
   return (
     <Layout title={'Home'}>
-      <h1 className="mt-4 pt-2 pb-4 border-bottom border-2">Filmes em alta</h1>
+      <h1 className="mt-4 pt-2 pb-4 border-bottom border-2">Popular movies</h1>
+      <InfiniteScroll
+        className="row"
+        pageStart={0}
+        loadMore={fetchMovies}
+        hasMore={hasMoreMovies}
+        loader={
+          <div className="d-flex" key={0}>
+            <Loader
+              className="mx-auto"
+              type="Puff"
+              color="#00BFFF"
+              height={100}
+              width={100}
+            />
+          </div>
+        }
+      >
+        {movies.map((movie) => (
+          <div className="col-2 mb-3" key={movie.id}>
+            <MovieCard movie={movie} hoverable />
+            <h6 className="mt-3 mb-1">{movie.title}</h6>
+            <p style={{ fontSize: '14px' }}>
+              <Moment date={movie.release_date} format="ll" />
+            </p>
+          </div>
+        ))}
+      </InfiniteScroll>
+      {!hasMoreMovies && (
+        <h5 className="text-center mb-5">No more movies to load.</h5>
+      )}
     </Layout>
   )
 }
--- src/services/moviesApi.js
+++ src/services/moviesApi.js
@@ -2,7 +2,7 @@ import api from './api'
 
 const moviesApi = () => ({
   getOne: (id) => api.get(`/movie/${id}`),
-  getPopular: () => api.get('/movie/popular'),
+  getPopular: (page) => api.get('/movie/popular', { params: { page: page } }),
   getSimilar: (id) => api.get(`/movie/${id}/similar`),
   getCredits: (id) => api.get(`/movie/${id}/credits`),
   getVideos: (id) => api.get(`/movie/${id}/videos`),
