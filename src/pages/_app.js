3028a5dda3ed2c2f7d9b100ea538029f1521a7fb
Pedro Endo <endopedro@gmail.com>
2021-05-27 21:34:03 -0300

    feat: nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,15 @@ import '../assets/styles/main.scss'
 
 import React from 'react'
 import Head from 'next/head'
+import Router from 'next/router'
 import { DefaultSeo } from 'next-seo'
+import NProgress from 'nprogress'
+
 import SEO from '../../next-seo.config'
+
+Router.events.on('routeChangeStart', (url) => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 const MyApp = ({ Component, pageProps }) => {
   return (
