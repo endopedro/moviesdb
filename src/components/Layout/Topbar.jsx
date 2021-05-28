93758165e23efa39876aa8f2a70edb0055668569
Pedro Endo <endopedro@gmail.com>
2021-05-27 21:34:42 -0300

    fix: topbar link

--- src/components/Layout/Topbar.jsx
+++ src/components/Layout/Topbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Topbar = () => {
-  return (
-    <nav className="topbar navbar border-top border-bottom shadow">
-      <div className="container">
-        <a className="logo navbar-brand fw-bold mx-auto" href="/">
+const Topbar = () => (
+  <nav className="topbar navbar border-top border-bottom shadow">
+    <div className="container">
+      <Link href="/">
+        <a className="logo navbar-brand fw-bold mx-auto">
           <span className="colored">MOVIES</span>
           <span className="text-white">DB</span>
         </a>
-      </div>
-    </nav>
-  )
-}
+      </Link>
+    </div>
+  </nav>
+)
 
 export default Topbar
